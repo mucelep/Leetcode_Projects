06d355da6cef34354a92ec5306760c77e2313032
Muhammed Ömer CELEP <celepm82@gmail.com>
2026-09-22 06:10:02 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (24.54%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        p: dict[str, str] = {
+            "(": ")",
+            "{": "}",
+            "[": "]"
+        }
+        stack: list[str] = []
+
+        for char in s:
+            if char in p:# tüm stringi gezip karakter açılışparantezi mi kontrolü
+                stack.append(p[char])# açılış parantezi ise karşılıgını listeye atıyoruz
+            else:
+                # daha sonra kapanış parantezine denk gelince eşleşip eşleşmedigine bakıyoruz
+                if not stack or stack.pop() != char:
+                    return False
+        return not stack
+
+
+
+
+# ({[ ]}) 
+# )}] listeye attıgında
+# 3. indeks else giriyor ] ile .pop() listenin son elemanına bakar
