6068778d7d1acb8cd5a78a54d9eb818bb0693eaf
Muhammed Ömer CELEP <celepm82@gmail.com>
2026-09-22 05:37:37 +0300

    Time: 0 ms (100.00%) | Memory: 8.9 MB (68.51%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.c
+++ 14-longest-common-prefix/longest-common-prefix.c
@@ -7,19 +7,19 @@ char* longestCommonPrefix(char** strs, int strsSize)
     result = malloc(strlen(strs[0]) + 1);
     i = 0;
 
-    while (strs[0][i])
+    while (strs[0][i])//referans bir kelime sec
     {
         j = 1;
-        while (j < strsSize)
+        while (j < strsSize)//ref kelimenin ilk harfi (i) ile diger kelimelerin harflerini kıyasla
         {
             if (strs[j][i] != strs[0][i])
             {
-                result[i] = '\0';
+                result[i] = '\0';// fark varsa null ekle bitir
                 return (result);
             }
             j++;
         }
-        result[i] = strs[0][i];
+        result[i] = strs[0][i];// fark yoksa sonuça ekle demekki aynı
         i++;
     }
     result[i] = '\0';
